f0afdd29a5b0b2434313d444313ab5ff36166d24
Kian Woo <kian.woo@gmail.com>
2015-09-29 22:07:02 +1000

    Continue with lesson 1

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -11,9 +11,9 @@ This is empty on purpose! Your code to build the resume will go here.
 			"twitter": "JianHu9",
 			"location" : "Sydney"
 		},
-		"pictureURL" : "images/me.jpg",
+		"pictureURL" : "images/fry.jpg",
 		"welcomeMessage" : "Welcome la la la",
-		"skills" : ["Javscript", "HTML", "CSS", "Photoshop"]
+		"skills" : ["Javascript", "HTML", "CSS", "Photoshop"]
 };
 
 
@@ -22,7 +22,33 @@ This is empty on purpose! Your code to build the resume will go here.
 
 	$("#header").prepend(formattedRole);
 	$("#header").prepend(formattedName);
+	$("#header").prepend(HTMLbioPic.replace("%data%", bio.pictureURL));
+	$("#header").append(HTMLwelcomeMsg.replace("%data%", bio.welcomeMessage));
+	
+/*
+var HTMLmobile = '<li class="flex-item"><span class="orange-text">mobile</span><span class="white-text">%data%</span></li>';
+var HTMLemail = '<li class="flex-item"><span class="orange-text">email</span><span class="white-text">%data%</span></li>';
+var HTMLtwitter = '<li class="flex-item"><span class="orange-text">twitter</span><span class="white-text">%data%</span></li>';
+var HTMLgithub = '<li class="flex-item"><span class="orange-text">github</span><span class="white-text">%data%</span></li>';
+var HTMLblog = '<li class="flex-item"><span class="orange-text">blog</span><span class="white-text">%data%</span></li>';
+var HTMLlocation = '<li class="flex-item"><span class="orange-text">location</span><span class="white-text">%data%</span></li>';
 
-	var formattedEmail = HTMLemail.replace("%data%", bio['contact info'].email);
+var HTMLbioPic = '<img src="%data%" class="biopic">';
+var HTMLwelcomeMsg = '<span class="welcome-message">%data%</span>';
 
-	$("#topContacts").append(formattedEmail);
+var HTMLskillsStart = '<h3 id="skills-h3">Skills at a Glance:</h3><ul id="skills" class="flex-box"></ul>';
+var HTMLskills = '<li class="flex-item"><span class="white-text">%data%</span></li>';
+*/
+
+	$("#topContacts").append(HTMLemail.replace("%data%", bio['contactInfo'].email));
+	$("#topContacts").append(HTMLmobile.replace("%data%", bio.contactInfo.mobile));
+	$("#topContacts").append(HTMLtwitter.replace("%data%", bio.contactInfo.twitter));
+	$("#topContacts").append(HTMLgithub.replace("%data%", bio.contactInfo.github));
+	$("#topContacts").append(HTMLlocation.replace("%data%", bio.contactInfo.location));
+
+	var formattedSkills = HTMLskillsStart;
+	for (var i=0; i<bio.skills.length; i+=1) {
+		formattedSkills = formattedSkills + HTMLskills.replace("%data%", bio.skills[i]);
+	}
+	$("#topContacts").append(formattedSkills);
+
