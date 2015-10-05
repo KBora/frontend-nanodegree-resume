5f086d34ccc3c868a6e6d07fd77b375161e1d45b
Kian Woo <kian.woo@gmail.com>
2015-10-05 13:35:51 +1100

    Lesson 2 - Add work details

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,83 +1,104 @@
-/*
-This is empty on purpose! Your code to build the resume will go here.
- */
- 	var bio = {
-		"name": "Bora Chan",
-		"role": "Front End Developer",
-		"contactInfo" : {
-			"email": "bora.chanello@gmail.com",
-			"mobile": "0421 432 543",
-			"github": "KBora",
-			"twitter": "JianHu9",
-			"location" : "Sydney"
+
+var work = {
+	"jobs": [
+		{
+			"employer": "Aliens Insurance",
+			"title": "Digital Content Analyst",
+			"datesWorked": "July 2015 - present",
+			"description": "Front end web development for the Aliens Insurance websites, intranet and extranet sites.",
+			"location": "Sydney"
 		},
-		"pictureURL" : "images/fry.jpg",
-		"welcomeMessage" : "Welcome la la la",
-		"skills" : ["Javascript", "HTML", "CSS", "Photoshop"]
+		{
+			"employer": "IBM",
+			"title": "Front End Developer",
+			"datesWorked": "May 2012 - Oct 2014",
+			"description": "Maintenance and enhancements to the web front end of software provisioning application for a large bank",
+			"location": "Sydney"
+		},
+		{
+			"employer": "Freelance",
+			"title": "Graphic Designer and Web Developer",
+			"datesWorked": "July 2005 - present",
+			"description": "Creation of marketing collateral. Customisation and custom development of websites for small to medium sized businesses using Wordpress, Shopify and BigCommerce",
+			"location": "Sydney"
+		},
+		{
+			"employer": "Sony Computer Entertainment Europe",
+			"title": "Intranet Developer",
+			"datesWorked": "February 2000 - May 2005",
+			"description": "Development and support of Intranet applications.",
+			"location": "London"
+		}
+	]
 };
 
 
-	var formattedName = HTMLheaderName.replace("%data%", bio.name);
-	var formattedRole = HTMLheaderRole.replace("%data%", bio.role);
+var bio = {
+	"name": "Bora Chan",
+	"role": "Front End Developer",
+	"contacts" : {
+		"email": "bora.chanello@gmail.com",
+		"mobile": "0421 432 543",
+		"github": "KBora",
+		"twitter": "JianHu9",
+		"location" : "Sydney"
+	},
+	"pictureURL" : "images/fry.jpg",
+	"welcomeMessage" : "Welcome la la la",
+	"skills" : ["Javascript", "HTML", "CSS", "Photoshop"]
+};
 
-	$("#header").prepend(formattedRole);
-	$("#header").prepend(formattedName);
-	$("#header").prepend(HTMLbioPic.replace("%data%", bio.pictureURL));
-	$("#header").append(HTMLwelcomeMsg.replace("%data%", bio.welcomeMessage));
+var education = {
+	"schools": [
+		{
+			"name": "Central Saint Martins College of Art and Design",
+			"location:": "London",
+			"degree": "BA",
+			"major": ["Graphic Design", "Illustration"],
+			"datesAttended": "2001 - 2004",
+			"url:": "http://www.arts.ac.uk/csm/"
+		},
+		{
+			"name": "University of Technology, Sydney",
+			"location:": "Sydney",
+			"degree": "Bachelor of Information Technology",
+			"datesAttended": "1998 - 2000",
+			"url" : "http://www.uts.edu.au/"
+		}
+	],
+	"onlineCourses": [
+		{
+			"title": "Human-Computer Interaction",
+			"school": "Stanford / Coursera",
+			"datesAttended": "2013",
+			"url": "http://online.stanford.edu/course/hci"
+		}
+	]
+};
+
+if (bio.skills.length > 0) {
 	
-/*
-var HTMLmobile = '<li class="flex-item"><span class="orange-text">mobile</span><span class="white-text">%data%</span></li>';
-var HTMLemail = '<li class="flex-item"><span class="orange-text">email</span><span class="white-text">%data%</span></li>';
-var HTMLtwitter = '<li class="flex-item"><span class="orange-text">twitter</span><span class="white-text">%data%</span></li>';
-var HTMLgithub = '<li class="flex-item"><span class="orange-text">github</span><span class="white-text">%data%</span></li>';
-var HTMLblog = '<li class="flex-item"><span class="orange-text">blog</span><span class="white-text">%data%</span></li>';
-var HTMLlocation = '<li class="flex-item"><span class="orange-text">location</span><span class="white-text">%data%</span></li>';
+	$("#header").append(HTMLheaderName.replace("%data%", bio.name));
+	$("#header").append(HTMLskillsStart);
 
-var HTMLbioPic = '<img src="%data%" class="biopic">';
-var HTMLwelcomeMsg = '<span class="welcome-message">%data%</span>';
-
-var HTMLskillsStart = '<h3 id="skills-h3">Skills at a Glance:</h3><ul id="skills" class="flex-box"></ul>';
-var HTMLskills = '<li class="flex-item"><span class="white-text">%data%</span></li>';
-*/
-
-	$("#topContacts").append(HTMLemail.replace("%data%", bio['contactInfo'].email));
-	$("#topContacts").append(HTMLmobile.replace("%data%", bio.contactInfo.mobile));
-	$("#topContacts").append(HTMLtwitter.replace("%data%", bio.contactInfo.twitter));
-	$("#topContacts").append(HTMLgithub.replace("%data%", bio.contactInfo.github));
-	$("#topContacts").append(HTMLlocation.replace("%data%", bio.contactInfo.location));
-
-	var formattedSkills = HTMLskillsStart;
 	for (var i=0; i<bio.skills.length; i+=1) {
-		formattedSkills = formattedSkills + HTMLskills.replace("%data%", bio.skills[i]);
+		$("#skills").append(HTMLskills.replace("%data%", bio.skills[i]));
 	}
-	$("#topContacts").append(formattedSkills);
-
-/* skills */
-var formattedSkills = HTMLskillsStart;
-for (var i=0; i<bio.skills.length; i+=1) {
-	formattedSkills = formattedSkills + HTMLskills.replace("%data%", bio.skills[i]);
 }
-$("#topContacts").append(formattedSkills);
 
-/* work */
-/*
-var HTMLworkStart = '<div class="work-entry"></div>';
-var HTMLworkEmployer = '<a href="#">%data%';
-var HTMLworkTitle = ' - %data%</a>';
-var HTMLworkDates = '<div class="date-text">%data%</div>';
-var HTMLworkLocation = '<div class="location-text">%data%</div>';
-var HTMLworkDescription = '<p><br>%data%</p>';
-*/
-HTMLworkEmployer = HTMLworkEmployer.replace("%data%", work.employer);
-HTMLworkTitle = HTMLworkTitle.replace("%data%", work.position);
-$("#workExperience").append(HTMLworkStart + HTMLworkEmployer + HTMLworkTitle);
+for (item in work.jobs) {
+	$("#workExperience").append(HTMLworkStart);
 
-/* education */
-/*
-var HTMLschoolStart = '<div class="education-entry"></div>';
-var HTMLschoolName = '<a href="#">%data%';
-var HTMLschoolDegree = ' -- %data%</a>';
-var HTMLschoolDates = '<div class="date-text">%data%</div>';
-var HTMLschoolLocation = '<div class="location-text">%data%</div>';
-var HTMLschoolMajor = '<em><br>Major: %data%</em>';
-*/
+	var formattedEmployerName = HTMLworkEmployer.replace("%data%", work.jobs[item].employer);
+	var formattedEmployerTitle = HTMLworkTitle.replace("%data%", work.jobs[item].title);
+	var formattedEmployerLocation = HTMLworkLocation.replace("%data%", work.jobs[item].location);
+	var formattedEmployerDates = HTMLworkDates.replace("%data%", work.jobs[item].datesWorked);
+	var formattedEmployerDescription = HTMLworkDescription.replace("%data%", work.jobs[item].description);
+
+	$(".work-entry:last").append(formattedEmployerName + formattedEmployerTitle);
+	$(".work-entry:last").append(formattedEmployerLocation);
+	$(".work-entry:last").append(formattedEmployerDates);
+	$(".work-entry:last").append(formattedEmployerDescription);
+
+}
+
