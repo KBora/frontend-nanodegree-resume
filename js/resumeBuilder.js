4b4ad7fa968ea4dbb29e5d675dad49fff6252431
wookkii <wookkii@home>
2016-03-12 20:06:07 +1100

    Move display schools into MVO

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -2,7 +2,7 @@ $(function(){ // this function is passed into jquery and run onload of the docum
 
 	var model = {
 
-		bio : {
+		bio: {
 			"name": "Jian Hu",
 			"role": "Graphic Designer",
 			"contacts" : {
@@ -17,7 +17,7 @@ $(function(){ // this function is passed into jquery and run onload of the docum
 			"skills" : ["Photoshop", "Illustrator", "InDesign", "HTML", "CSS", "Javascript"]
 		},
 
-		jobs : [
+		jobs: [
 			{
 				"employer": "Aliens Insurance",
 				"title": "Digital Content Analyst",
@@ -41,7 +41,7 @@ $(function(){ // this function is passed into jquery and run onload of the docum
 			}
 		],
 
-		"schools": [
+		schools: [
 			{
 				"name": "Central Saint Martins College of Art and Design",
 				"location": "London",
@@ -52,7 +52,7 @@ $(function(){ // this function is passed into jquery and run onload of the docum
 			},
 		],
 
-		"onlineCourses": [
+		onlineCourses: [
 			{
 					"title": "Human-Computer Interaction",
 					"school": "Stanford / Coursera",
@@ -68,11 +68,7 @@ $(function(){ // this function is passed into jquery and run onload of the docum
 				"description": "Logo creation, product photography, editorial photography.",
 				"images": ["images/sideralia.png"]
 			}
-		],
-
-		getBio : function() {
-			return model.bio;
-		}
+		]
 
 	};
 
@@ -81,6 +77,18 @@ $(function(){ // this function is passed into jquery and run onload of the docum
 
 		init: function() {
 			view.init()
+		},
+
+		getBio: function() {
+			return model.bio;
+		},
+
+		getJobs: function() {
+			return model.jobs;
+		},
+
+		getSchools: function() {
+			return model.schools;
 		}
 
 	};
@@ -89,12 +97,14 @@ $(function(){ // this function is passed into jquery and run onload of the docum
 	var view = {
 
 		init: function() {
-			// TO DO: Display bio, jobs, schools, online courses
+			// TO DO: Display  schools, online courses
 			view.displayBio();
+			view.displayJobs();
+			view.displaySchools();
 		},
 
 		displayBio: function() {
-			var bioData = model.getBio();
+			var bioData = octopus.getBio();
 			var formattedName = HTMLheaderName.replace("%data%", bioData.name);		
 			var formattedRole = HTMLheaderRole.replace("%data%", bioData.role);
 			$("#header").prepend(formattedRole);
@@ -121,10 +131,42 @@ $(function(){ // this function is passed into jquery and run onload of the docum
 		},
 
 		displayJobs: function() {
+			var workData = octopus.getJobs();
+			for (var item in workData) {
+				$("#workExperience").append(HTMLworkStart);
 
+				var formattedEmployerName = HTMLworkEmployer.replace("%data%", workData[item].employer);
+				var formattedEmployerTitle = HTMLworkTitle.replace("%data%", workData[item].title);
+				var formattedEmployerLocation = HTMLworkLocation.replace("%data%", workData[item].location);
+				var formattedEmployerDates = HTMLworkDates.replace("%data%", workData[item].dates);
+				var formattedEmployerDescription = HTMLworkDescription.replace("%data%", workData[item].description);
+
+				$(".work-entry:last").append(formattedEmployerName + formattedEmployerTitle);
+				$(".work-entry:last").append(formattedEmployerLocation);
+				$(".work-entry:last").append(formattedEmployerDates);
+				$(".work-entry:last").append(formattedEmployerDescription);	
+			}
 		},
 
 		displaySchools: function() {
+
+			var schoolsData = octopus.getSchools();
+			for (var school in schoolsData) {
+				$("#education").append(HTMLschoolStart);
+				var formattedSchoolName = HTMLschoolName.replace("%data%", schoolsData[school].name);
+				formattedSchoolName = formattedSchoolName.replace("#", schoolsData[school].url);
+				var formattedSchoolDegree = HTMLschoolDegree.replace("%data%", schoolsData[school].degree);
+				var formattedSchoolDates = HTMLschoolDates.replace("%data%", schoolsData[school].date);
+				var formattedSchoolLocation = HTMLschoolLocation.replace("%data%", schoolsData[school].location);
+				$(".education-entry:last").append(formattedSchoolName + formattedSchoolDegree);
+				$(".education-entry:last").append(formattedSchoolDates);
+				$(".education-entry:last").append(formattedSchoolLocation);
+				for (var major in schoolsData[school].major) {
+					var formattedSchoolMajor = HTMLschoolMajor.replace("%data%", schoolsData[school].major[major]);
+					$(".education-entry:last").append(formattedSchoolMajor);
+				}
+
+		}
 
 		},
 
@@ -144,128 +186,13 @@ $(function(){ // this function is passed into jquery and run onload of the docum
 
 });
 
-var bio = {
-	"name": "Jian Hu",
-	"role": "Graphic Designer",
-	"contacts" : {
-		"email": "akp@evenfeatures.com",
-		"mobile": "0421 432 XXX",
-		"github": "KBora",
-		"twitter": "JianHu9",
-		"location": "Sydney, Australia"
-	},
-	"biopic" : "images/jian-hu-profile.png",
-	"welcomeMessage" : "Welcome to my resume",
-	"skills" : ["Photoshop", "Illustrator", "InDesign", "HTML", "CSS", "Javascript"],
-	display: function() {
-
-		var formattedName = HTMLheaderName.replace("%data%", bio.name);		
-		var formattedRole = HTMLheaderRole.replace("%data%", bio.role);
-		$("#header").prepend(formattedRole);
-		$("#header").prepend(formattedName);
-
-		for (var contact in bio.contacts) {
-			var formattedContactGeneric = HTMLcontactGeneric.replace("%data%", bio.contacts[contact]).replace("%contact%", contact);
-			$("#topContacts").append(formattedContactGeneric);
-			$("#footerContacts").append(formattedContactGeneric);
-		}
-
-		var formattedBioPic = HTMLbioPic.replace("%data%", bio.biopic );
-		var formattedWelcome = HTMLwelcomeMsg.replace("%data%", bio.welcomeMessage);
-		$("#header").append(formattedBioPic);
-		$("#header").append(formattedWelcome);
-		
-
-		if (bio.skills.length > 0) {
-			$("#header").append(HTMLskillsStart);
-			for (var i=0; i<bio.skills.length; i+=1) {
-				$("#skills").append(HTMLskills.replace("%data%", bio.skills[i]));
-			}
-		}
-
-	}
-};
-
-var work = {
-	"jobs": [
-		{
-			"employer": "Aliens Insurance",
-			"title": "Digital Content Analyst",
-			"dates": "July 2015 - present",
-			"description": "Front end web development for the Aliens Insurance websites, intranet and extranet sites.",
-			"location": "Sydney"
-		},
-		{
-			"employer": "Freelance",
-			"title": "Graphic Designer and Web Developer",
-			"dates": "July 2005 - present",
-			"description": "Creation of marketing collateral. Customisation of websites for small to medium sized businesses using Wordpress, Shopify and BigCommerce.",
-			"location": "Sydney"
-		},
-		{
-			"employer": "Sony Computer Entertainment Europe",
-			"title": "Tea boy",
-			"dates": "February 2000 - May 2005",
-			"description": "Made tea.",
-			"location": "London"
-		}
-	], 
-	display: function() {
-		for (var item in work.jobs) {
-			$("#workExperience").append(HTMLworkStart);
-
-			var formattedEmployerName = HTMLworkEmployer.replace("%data%", work.jobs[item].employer);
-			var formattedEmployerTitle = HTMLworkTitle.replace("%data%", work.jobs[item].title);
-			var formattedEmployerLocation = HTMLworkLocation.replace("%data%", work.jobs[item].location);
-			var formattedEmployerDates = HTMLworkDates.replace("%data%", work.jobs[item].dates);
-			var formattedEmployerDescription = HTMLworkDescription.replace("%data%", work.jobs[item].description);
-
-			$(".work-entry:last").append(formattedEmployerName + formattedEmployerTitle);
-			$(".work-entry:last").append(formattedEmployerLocation);
-			$(".work-entry:last").append(formattedEmployerDates);
-			$(".work-entry:last").append(formattedEmployerDescription);	
-		}
-	}
-};
 
 
 
 var education = {
-	"schools": [
-		{
-			"name": "Central Saint Martins College of Art and Design",
-			"location": "London",
-			"degree": "BA",
-			"major": ["Graphic Design", "Illustration"],
-			"date": "2004",
-			"url": "http://www.arts.ac.uk/csm/"
-		},
-	],
-	"onlineCourses": [
-		{
-			"title": "Human-Computer Interaction",
-			"school": "Stanford / Coursera",
-			"date": "2013",
-			"url": "http://online.stanford.edu/course/hci"
-		}
-	],
-	display: function() {
-		for (var school in this.schools) {
-			$("#education").append(HTMLschoolStart);
-			var formattedSchoolName = HTMLschoolName.replace("%data%", this.schools[school].name);
-			formattedSchoolName = formattedSchoolName.replace("#", this.schools[school].url);
-			var formattedSchoolDegree = HTMLschoolDegree.replace("%data%", this.schools[school].degree);
-			var formattedSchoolDates = HTMLschoolDates.replace("%data%", this.schools[school].date);
-			var formattedSchoolLocation = HTMLschoolLocation.replace("%data%", this.schools[school].location);
-			$(".education-entry:last").append(formattedSchoolName + formattedSchoolDegree);
-			$(".education-entry:last").append(formattedSchoolDates);
-			$(".education-entry:last").append(formattedSchoolLocation);
-			for (var major in this.schools[school].major) {
-				var formattedSchoolMajor = HTMLschoolMajor.replace("%data%", this.schools[school].major[major]);
-				$(".education-entry:last").append(formattedSchoolMajor);
-			}
 
-		}
+	display: function() {
+
 
 		$("#education").append(HTMLonlineClasses);
 		for (var onlineCourse in this.onlineCourses) {
@@ -320,9 +247,9 @@ function inName(nameString) {
 /* $("#main").append(internationalizeButton); */
 
 //bio.display();
-work.display();
+//work.display();
 projects.display();
-education.display();
+//education.display();
 
 $("#mapDiv").append(googleMap);
 
